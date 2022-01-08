4abb16a63772285f09dddd093ee6031a7e3ff076
飯塚晃生 <kou605088@icloud.com>
2022-01-08 16:59:28 +0900

    main

---
+++ discordbot.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+token = os.environ['DISCORD_BOT_TOKEN']
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='/',intents = intents)
+
+
+logchannel = 928951765867585536
+
+
+@bot.event
+async def on_ready():
+    print('Ready!')
+    channel = bot.get_channel(logchannel)
+    await bot.send.channel('logined')
+
+@bot.listen()
+async def on_message(message):
+    print('one')
+
+
+bot.login(token)
