c34a85246a6527317bbc9377bd07900539e8aa9a
飯塚晃生 <kou605088@icloud.com>
2022-01-11 11:32:40 +0900

    ログイン時のプログラムの設定変更

--- discordbot.py
+++ discordbot.py
@@ -12,15 +12,20 @@ bot = commands.Bot(command_prefix='/',intents = intents)
 logchannel = 928951765867585536
 
 
+async def great():
+    channel = bot.get_channel(logchannel)
+    await channel.send('logined')
+    return
+
 @bot.event
 async def on_ready():
     print('Ready!')
-    channel = bot.get_channel(logchannel)
-    await bot.send.channel('logined')
+    await great()
+    return
 
 @bot.listen()
 async def on_message(message):
     print('one')
-
+    return
 
 bot.run(token)
