8c4346db970e68284ea58115462138dcfbfa05dc
飯塚晃生 <kou605088@icloud.com>
2022-01-11 11:54:34 +0900

    a

--- discordbot.py
+++ discordbot.py
@@ -6,7 +6,7 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 intents = discord.Intents.all()
 
-bot = commands.Bot(command_prefix='/',intents = intents)
+bot = commands.Bot(command_prefix='&',intents = intents)
 
 
 bot.run(token)
