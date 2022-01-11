68c111ae11f97e204bf3d0a74f47a145ae87db89
飯塚晃生 <kou605088@icloud.com>
2022-01-11 11:43:39 +0900

    実験

--- discordbot.py
+++ discordbot.py
@@ -9,23 +9,4 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='/',intents = intents)
 
 
-logchannel = 928951765867585536
-
-
-async def great():
-    channel = bot.get_channel(logchannel)
-    await channel.send('logined')
-    return
-
-@bot.event
-async def on_ready():
-    print('Ready!')
-    await great()
-    return
-
-@bot.listen()
-async def on_message(message):
-    print('one')
-    return
-
 bot.run(token)
